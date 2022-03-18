380728f54827926eff79bc4ea72ed9d0d873f607
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-18 18:16:01 +0400

    added @Scope, @Value, @PostConstruct, @PreDestroy

--- src/main/java/com/spring/framework/ClassicalMusic.java
+++ src/main/java/com/spring/framework/ClassicalMusic.java
@@ -1,9 +1,22 @@
 package com.spring.framework;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
