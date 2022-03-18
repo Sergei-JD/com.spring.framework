2bf908015a53edd0b83b83caedebf5b84eebaa5d
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-18 15:46:47 +0400

    implemented @Autowired (nested dependencies)

--- src/main/java/com/spring/framework/TestSpring.java
+++ src/main/java/com/spring/framework/TestSpring.java
@@ -9,13 +9,19 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music1 = context.getBean("rockMusic", Music.class);
-        MusicPlayer rockMusicPlayer = new MusicPlayer(music1);
-        rockMusicPlayer.playMusic();
+//        Music music1 = context.getBean("rockMusic", Music.class);
+//        MusicPlayer rockMusicPlayer = new MusicPlayer(music1);
+//        rockMusicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-        classicalMusicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
